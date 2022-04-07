382f012f3c85dc5ca34b656d3e024436388d984d
Victoriano <mytacam78@gmail.com>
2022-04-07 00:49:02 -0600

    education and work

--- alexa/js/main.js
+++ alexa/js/main.js
@@ -35,8 +35,19 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 
 
 function toggleSkills(){
-  let 
+  let itemClass  = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
 }
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills)
+})
 
 
 
