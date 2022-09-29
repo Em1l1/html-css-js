9383a3080d2a857467114ecfc9067810702fcde1
Emily <75467880+Em1l1@users.noreply.github.com>
2022-09-29 00:44:47 -0600

    add style popup info

--- portfolio/john/js/script.js
+++ portfolio/john/js/script.js
@@ -14,3 +14,34 @@ tabsContainer.addEventListener("click", (e) => {
     aboutSection.querySelector(target).classList.add("active");
   }
 });
+
+
+// Porfolio Item Details Popup
+document.addEventListener("click", (e) => {
+  if(e.target.classList.contains("view-project-btn")){
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+})
+
+function togglePortfolioPopup(){
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+  if(e.target.classList.contains("pp-inner")){
+    togglePortfolioPopup();
+  };
+})
+
+function portfolioItemDetails(portfolioItem){
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src
+
+  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
