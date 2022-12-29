a0022fa15218ea9a7d6485544310f08c03cf10b6
Emily <blancocronos@hotmail.com>
2022-12-28 19:54:43 -0600

    click button js

--- portfolio/john/js/script.js
+++ portfolio/john/js/script.js
@@ -1,4 +1,41 @@
+/*
+  ----------------- Toggle Navbar ---------------------------
+*/ 
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
 
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+/* ------------------------ Active section ------------------------- */
+document.addEventListener("click", (e) => {
+  if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+    // active the overlay to prevent multiple clicks
+    document.querySelector(".overlay").classList.add("active")
+    if(e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document.querySelector("section.active").classList.remove("active","fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0,0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    },500);
+  }
+});
 
 /* --------------------------- About Tabs ----------------------------------- */
 const tabsContainer = document.querySelector(".about-tabs"),
@@ -46,3 +83,5 @@ function portfolioItemDetails(portfolioItem){
 
   document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
+
+
