a59a8ec4e242af8acd794450d3e701b83405a251
Emily <blancocronos@hotmail.com>
2024-01-11 21:47:00 -0600

    portfolio doe

---
+++ portfolio/doe/main.js
@@ -0,0 +1,35 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+    
+
+
+/* ----- TYPING EFFECT ----- */
+   
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+   
+
+  /* -- HOME -- */
+  
+
+  /* -- PROJECT BOX -- */
+  
+
+  /* -- HEADINGS -- */
+  
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+  /* -- ABOUT INFO & CONTACT INFO -- */
+  
+
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+  
+  
+   
