968d3b635784dc69b1889d1dee8dda60505b593e
Emily <blancocronos@hotmail.com>
2024-01-11 22:36:11 -0600

    Chore(portfolio) navbar

--- portfolio/doe/assets/js/main.js
+++ portfolio/doe/assets/js/main.js
@@ -1,5 +1,13 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
+  var menuBtn = document.getElementById("myNavMenu");
 
+  if(menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
     
